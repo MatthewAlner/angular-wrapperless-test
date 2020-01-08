8f9066f238a0a0bcc801da4fc4b062b694a98485
Matthew Alner <matthew.alner@senseye.io>
2020-01-08 14:27:52 +0000

    added remove wrapper directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RemoveWrapperDirective } from './remove-wrapper.directive';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RemoveWrapperDirective,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule
